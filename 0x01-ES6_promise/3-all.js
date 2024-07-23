1ff4398f136594f16e105e2e4f0d05737c13dade
essaket <hichamessaket17@gmail.com>
2024-07-23 12:20:21 -0700

    Writing task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,6 +3,6 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((result) => console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`))
-    .catch(() => new Error('Signup system offline'));
+    .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
