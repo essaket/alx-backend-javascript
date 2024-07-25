7d4e807f1311016820a4a27f1a65b6bb455cb393
essaket <hichamessaket17@gmail.com>
2024-07-25 08:16:12 -0700

    Writing task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  /* Gets ana Sets of name */
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  /* Gets and Sets of length */
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    this._length = value;
+  }
+
+  /* Gets and Sets of students */
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) || value.every((student) => typeof student !== 'string')) {
+      throw new TypeError('Student must be an array of strings');
+    }
+    this._students = students;
+  }
+}
