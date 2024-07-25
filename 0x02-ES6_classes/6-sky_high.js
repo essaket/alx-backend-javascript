3f498bc85cab0d0ce543cce851c55f46bc9ab121
essaket <hichamessaket17@gmail.com>
2024-07-25 12:30:39 -0700

    Writing task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  /* Gets and Sets for floors */
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  /* Method return a string */
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
