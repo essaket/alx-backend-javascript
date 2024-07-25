e9ca71707679a2bf13636787bdc4e11d16bc1e02
essaket <hichamessaket17@gmail.com>
2024-07-25 12:09:41 -0700

    Writing task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,20 @@
 export default class Building {
   constructor(sqft) {
+  if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
+  /* Gets and Sets for sqft */
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+}
