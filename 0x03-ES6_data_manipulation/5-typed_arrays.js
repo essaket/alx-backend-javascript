172766a953111542cbf45d0aad12f7f31f688aa4
essaket <hichamessaket17@gmail.com>
2024-07-30 12:13:58 -0700

    Writing task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,6 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0) throw new Error('Position outside range');
+  const int8DataView = new DataView(new ArrayBuffer(length));
+  int8DataView.setInt8(position, value);
+  return int8DataView;
+}
