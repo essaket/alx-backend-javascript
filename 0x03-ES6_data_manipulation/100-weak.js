e4a10922e0e73e463492d12c6deef07d6425afb8
essaket <hichamessaket17@gmail.com>
2024-07-30 14:21:52 -0700

    Writing task 11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,12 @@
 const weakMap = new WeakMap();
 export default function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+  if (weakMap.get(endPoint) + 1) {
+    weakMap.set(endPoint, weakMap.get(endPoint) + 1);
+  } else {
+    weakMap.set(endPoint, 1);
   }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+  if (weakMap.get(endPoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
+  return weakMap;
 }
