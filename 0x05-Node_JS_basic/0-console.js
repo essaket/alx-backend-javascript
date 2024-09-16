0e1698bbe3ad052dbafe4a457cb4a8f2d5904517
essaket <hichamessaket17@gmail.com>
2024-09-16 06:05:23 -0700

    remove dir 0x05...

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,9 +0,0 @@
-/**
- * 0. Executing basic javascript with Node JS
- * Prints in STDOUT the string argument
- */
-function displayMessage(message) {
-    console.log(message);
-}
-
-module.exports = displayMessage;
