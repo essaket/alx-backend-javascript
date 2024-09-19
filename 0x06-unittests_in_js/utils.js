d1b6d7abc4b093f0fc7309b67f558ec5c5d35d77
essaket <hichamessaket17@gmail.com>
2024-09-19 08:34:42 -0700

    Writing task 3

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,17 @@
+/**
+ * 3. Spies
+ */
+
+const Utils = {
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') return Math.round(a) + Math.round(b);
+    if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
+    if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) return 'Error';
+      return Math.round(a) / Math.round(b);
+    }
+    return undefined;
+  }
+};
+
+module.exports = Utils;
